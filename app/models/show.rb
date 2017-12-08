46db9491c70f845e0472c74eee2c3dcf35f7d93c
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-08 18:24:01 +0000

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,34 @@
+require 'pry'
+
+class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    self.maximum("rating")
+  end
+
+  def self.most_popular_show
+    rating = self.highest_rating
+    self.find_by(rating: rating)
+  end
+
+  def self.lowest_rating
+    self.minimum("rating")
+  end
+
+  def self.least_popular_show
+    rating = self.lowest_rating
+    self.find_by(rating: rating)
+  end
+
+  def self.ratings_sum
+    self.sum('rating')
+  end
+
+  def self.popular_shows
+    self.order(rating: :desc).where("rating > ?", 5)
+  end
+
+  def self.shows_by_alphabetical_order
+    self.order(:name)
+  end
+end
