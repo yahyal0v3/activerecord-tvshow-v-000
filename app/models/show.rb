ced0b13cd0d2988439e0d3b78a5fa40aac6bfa4f
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2017-12-08 18:07:22 +0000

    automatically backed up by learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -14,4 +14,9 @@ class Show < ActiveRecord::Base
   def self.lowest_rating
     self.minimum("rating")
   end
+
+  def self.least_popular_show
+    rating = self.lowest_rating
+    self.find_by(rating: rating)
+  end
 end
